16e57e739e6d2488a3d81ad74a19d0b60eb216c3
Rodrigo <alddeacabos@gmail.com>
2026-08-28 14:40:45 -0700

    Clean up agent scripts

--- fix_brochures.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-let content = fs.readFileSync('app/actions/brochures.ts', 'utf8');
-content = content.replace('export async function createBrochure(data: { nombre: string, file_url: string }) {', 'export async function createBrochure(data: { nombre: string, file_url: string, file_base64?: string }) {');
-content = content.replace('file_url: data.file_url,', 'file_url: data.file_url,\n      file_base64: data.file_base64,');
-fs.writeFileSync('app/actions/brochures.ts', content);
-console.log('Modified actions');
-
